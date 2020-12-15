e51055f259e05379b95a69f34c2fa77f5941fda7
Hasmukh Rathod <hasmukhrathod@gmail.com>
2020-12-15 09:34:43 +0900

    output example added.

---
+++ basics/output/main.tf
@@ -0,0 +1,28 @@
+resource "random_uuid" "id1" {
+
+}
+resource "random_uuid" "id2" {
+
+}
+resource "random_uuid" "id3" {
+
+}resource "random_uuid" "id4" {
+
+}resource "random_uuid" "id5" {
+}
+resource "random_uuid" "id6" {
+
+}
+resource "random_uuid" "id7" {
+
+}
+resource "random_integer" "order1" {
+  min     = 1
+  max     = 99999
+
+}
+resource "random_integer" "order2" {
+  min     = 1
+  max     = 222222
+
+}
---
+++ basics/output/output.tf
@@ -0,0 +1,35 @@
+output "id1" {
+   value = random_uuid.id1.result
+}
+output "id2" {
+    value = random_uuid.id2.result
+
+}
+output "id3" {
+    value = random_uuid.id3.result
+
+}
+
+output "id4" {
+    value = random_uuid.id4.result
+
+}
+output "id5" {
+    value = random_uuid.id5.result
+}
+
+output "id6" {
+    value = random_uuid.id6.result
+
+}
+output "id7" {
+    value = random_uuid.id7.result
+}
+output "order1" {
+ value = random_integer.order1.result
+
+}
+output "order2" {
+ value = random_integer.order1.result
+
+}
