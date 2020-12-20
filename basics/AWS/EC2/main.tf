997e6b737c69d374584efdd460bdd308d4b3f34f
Hasmukh Rathod <hasmukhrathod@gmail.com>
2020-12-20 12:44:26 +0900

    aws_key_pair resource type added.

--- basics/AWS/EC2/main.tf
+++ basics/AWS/EC2/main.tf
@@ -3,3 +3,9 @@ resource "aws_instance" "web-server" {
   instance_type = "m5.large"
 
 }
+
+resource "aws_key_pair" "my-public-key" {
+  key_name = "web-server-public-key"
+  public_key = file("PATH_TO_PUBLIC_KEY.pub")
+
+}
