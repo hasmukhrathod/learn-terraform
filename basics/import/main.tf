02d0753636850ebba66fac575f1c479198caa630
Hasmukh Rathod <hasmukhrathod@gmail.com>
2020-12-22 11:36:59 +0900

    import example added

---
+++ basics/import/main.tf
@@ -0,0 +1,10 @@
+resource "aws_instance" "ruby" {
+  ami           = var.ami
+  instance_type = var.instance_type
+  for_each      = var.name
+  key_name      = var.key_name
+  tags = {
+    Name = each.value
+  }}
+output "instances" {
+  value = aws_instance.ruby}
